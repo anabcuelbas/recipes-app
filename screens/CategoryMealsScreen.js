bf6a62387c8ce63d7785f99a08ced023055e03e7
anabcuelbas <abcuelbas@gmail.com>
2020-09-23 11:33:01 -0300

    Quick fix on the background color

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -51,7 +51,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        marginVertical: 10
+        backgroundColor: 'white'
     }
 })
 
