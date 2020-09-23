bd192d03ad451b80071a74d9be4f110515422f0b
anabcuelbas <abcuelbas@gmail.com>
2020-09-23 11:56:41 -0300

    Added Bottom Tab Navigator

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,10 +1,14 @@
+import React from 'react'
 import { Platform } from 'react-native'
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
+import { createBottomTabNavigator } from 'react-navigation-tabs'
+import { Ionicons } from '@expo/vector-icons'
 
 import CategoriesScreen from '../screens/CategoriesScreen'
 import CategoryMealsScreen from '../screens/CategoryMealsScreen'
 import MealDetailScreen from '../screens/MealDetailScreen'
+import FavoritesScreen from '../screens/FavoritesScreen'
 import Colors from '../constants/Colors'
 
 const MealsNavigator = createStackNavigator({
@@ -22,4 +26,21 @@ const MealsNavigator = createStackNavigator({
     }
 })
 
-export default createAppContainer(MealsNavigator)
+const MealsFavTabNavigator = createBottomTabNavigator({
+    Meals: {screen: MealsNavigator, navigationOptions: {
+        tabBarIcon: (tabInfo) => {
+            return <Ionicons name="ios-restaurant" size={25} color={tabInfo.tintColor} />
+        }
+    }},
+    Favorites: {screen: FavoritesScreen, navigationOptions: {
+        tabBarIcon: (tabInfo) => {
+            return <Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />
+        }
+    }}
+}, {
+    tabBarOptions: {
+        activeTintColor: Colors.accentColor
+    }
+})
+
+export default createAppContainer(MealsFavTabNavigator)
