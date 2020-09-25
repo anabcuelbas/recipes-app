7677fca34e525298098c27d28015fd8b6245ac01
anabcuelbas <abcuelbas@gmail.com>
2020-09-25 15:42:59 -0300

    Added Redux

--- App.js
+++ App.js
@@ -3,10 +3,18 @@ import { enableScreens } from 'react-native-screens'
 
 import * as Font from 'expo-font'
 import { AppLoading } from 'expo'
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
 
 import MealsNavigator from './navigation/MealsNavigator'
+import mealsReducer from './store/reducers/meals'
 
 enableScreens()
+
+const rootReducer = combineReducers({
+  meals: mealsReducer
+})
+const store = createStore(rootReducer)
 
 const fetchFonts = () => {
   return Font.loadAsync({
@@ -27,6 +35,10 @@ export default function App() {
     )
   }
 
-  return <MealsNavigator />
+  return (
+    <Provider store={store}>
+      <MealsNavigator />
+    </Provider>
+  )
 }
 
